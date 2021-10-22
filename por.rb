a8a8ab6d21c9844b145250b2bb94436ed7009774
Qasim Abdullah <qasim.abdullah@travis-ci.org>
2021-10-22 18:14:36 +0500

    Brushed up

--- por.rb
+++ por.rb
@@ -3,9 +3,9 @@ require 'sidekiq'
 class PlainOldRuby
   include Sidekiq::Worker
 
-  def perform(how_hard="Super hard", how_long=5)
-    puts "Sleeping for #{how_long}"
+  def perform(message="Slept like a baby for", how_long=5)
+    puts "Sleeping for #{how_long} seconds. See ya!"
     sleep how_long
-    puts "Working #{how_hard}"
+    puts "#{message}"
   end
 end
